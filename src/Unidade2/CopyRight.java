dbdb5c1c571a02370e2c5fd9efb5c705e35c7078
WagnerSolak <wwwagnerww@gmail.com>
2019-12-04 22:16:53 -0300

    Anotacoes

---
+++ src/Unidade2/CopyRight.java
@@ -0,0 +1,8 @@
+package Unidade2;
+
+public @interface CopyRight {
+	
+	String autor();
+	String data();
+	String versao();
+}
---
+++ src/Unidade2/InternetApp.java
@@ -0,0 +1,19 @@
+package Unidade2;
+
+@CopyRight(autor="Wagner Solak", data="01/01/2020", versao="1.0")
+public class InternetApp implements Internet {
+
+	//@Override
+	@Deprecated
+	public void conectar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void conectarSSL() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
