bd81a7b7b48c7c7c2c701a8e5a5e940f8a3f750b
WagnerSolak <wwwagnerww@gmail.com>
2020-01-23 15:26:08 -0300

    Interfaces Queue e Dequeue

---
+++ src/Unidade4/DequeApp.java
@@ -0,0 +1,26 @@
+package Unidade4;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DequeApp {
+
+	public static void main(String[] args) {
+		Deque<String> lista = new ArrayDeque<>();
+		lista.add("vermelho");
+		lista.add("verde");
+		lista.add("verde");
+		lista.add("amarelo");
+
+		lista.removeFirst();
+		lista.removeLast();
+		 System.out.println("LISTA DE CORES: " +lista);
+		 
+		lista.addFirst("azul");
+		lista.addLast("preto");
+		
+		 System.out.println(lista.peekFirst());
+		 System.out.println(lista.peekLast());
+	}
+
+}
