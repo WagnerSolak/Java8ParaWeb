5b6cbb5361f2e4a55fa19165edacae2f81073de8
WagnerSolak <wwwagnerww@gmail.com>
2020-01-31 20:51:53 -0300

    Biblioteca Gráficas

---
+++ src/Unidade6/ContentApp.java
@@ -0,0 +1,14 @@
+package Unidade6;
+
+import javax.swing.JFrame;
+
+public class ContentApp {
+
+	public static void main(String[] args) {
+		
+		//criar o container (Local que recebe os componentes gráficos)
+		JFrame frame = new JFrame("First Aplicattion");
+		frame.setSize(350, 250);
+		frame.setVisible(true);
+	}
+}
